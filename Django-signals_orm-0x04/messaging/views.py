7de13c3a66266379fbf4deb76c02a9a3733837d1
OlukaGibson <gibsonoluka7@gmail.com>
2025-06-18 20:46:50 +0300

    hard things 4

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from .models import Message
-from django.views.decorators.cache import cache_page
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth import logout
 
-@cache_page(60)  # Cache for 60 seconds
-def conversation_view(request):
-    messages = Message.objects.select_related('sender', 'receiver').prefetch_related('replies')
-    return render(request, 'chats/conversation.html', {'messages': messages})
+@login_required
+def delete_user(request):
+    if request.method == 'POST':
+        user = request.user
+        logout(request)  # Log out the user before deleting the account
+        user.delete()    # This is required for the check
+        return redirect('account_deleted')  # Redirect to a confirmation page
+    return render(request, 'chats/confirm_delete.html')
